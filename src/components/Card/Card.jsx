b1ea7e68102a66ce4f4932583afdf43b165fe3c1
Matt Volonnino <mvolonnino12@gmail.com>
2020-12-17 00:00:41 -0500

    init repo with login and sign up pages w/ assets

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Card() {
+  return <div className="clash_card"></div>;
+}
+
+export default Card;
---
+++ src/pages/LoginPage/components/Login/Login.jsx
@@ -0,0 +1,160 @@
+import React, { useState } from "react";
+import { MDBAnimation } from "mdbreact";
+
+import "./Login.css";
+import clashLogin from "../../../../assets/clash-login-2.svg";
+import miniPekka from "../../../../assets/mini_pekka.png";
+import archer from "../../../../assets/archers.png";
+
+function Login() {
+  const [signup, setSignup] = useState(false);
+  const [userObj, setUserObj] = useState({});
+
+  const handleSignInLogin = () => {
+    setSignup(!signup);
+    setUserObj({});
+  };
+
+  return (
+    <div className="login-wrap">
+      <div className="row">
+        <div className="col-md-8">
+          {/* <MDBAnimation type="fadeInLeft" duration="1.8s"> */}
+          <div className="left-panel">
+            <div className="clash-illastration">
+              <MDBAnimation type="fadeInDown" delay="1s">
+                <img
+                  src={clashLogin}
+                  alt="Barbarians"
+                  className="image img-fluid"
+                />
+              </MDBAnimation>
+            </div>
+            <MDBAnimation type="fadeInUp" delay="1s">
+              <h2 className="motto">Clash Royale Stats Made Easy</h2>
+            </MDBAnimation>
+          </div>
+          {/* </MDBAnimation> */}
+        </div>
+        {signup && (
+          <div className={`col-md-4`}>
+            <MDBAnimation type="fadeInRight" duration="1s">
+              <div className="right-panel">
+                <h1>
+                  <strong>Time to join,</strong> <br />
+                  Clashers!
+                </h1>
+                <form className="animated-form">
+                  <h3>Create your account</h3>
+                  <div
+                    className={`form-group ${
+                      userObj?.email?.length > 0 ? "has-content" : ""
+                    }`}
+                  >
+                    <label htmlFor="email">Email</label>
+                    <input
+                      type="text"
+                      id="email"
+                      onChange={(e) =>
+                        setUserObj({ ...userObj, email: e.target.value })
+                      }
+                    />
+                  </div>
+                  <div
+                    className={`form-group ${
+                      userObj?.password?.length > 0 ? "has-content" : ""
+                    }`}
+                  >
+                    <label htmlFor="password">Password</label>
+                    <input
+                      type="password"
+                      id="password"
+                      onChange={(e) =>
+                        setUserObj({ ...userObj, password: e.target.value })
+                      }
+                    />
+                  </div>
+                  <div className="form-group flex-end">
+                    <button className="button">SIGNUP</button>
+                  </div>
+                  <MDBAnimation type="fadeInUp" delay="1s">
+                    <div className="new-user">
+                      <img src={archer} alt="miner" className="clash-pic" />
+                      <hr className="line" />
+                      <h3>Already a member?</h3>
+                      <p className="link" onClick={handleSignInLogin}>
+                        Login
+                      </p>
+                    </div>
+                  </MDBAnimation>
+                </form>
+              </div>
+            </MDBAnimation>
+          </div>
+        )}
+        {!signup && (
+          <div className={`col-md-4 slide-left`}>
+            <MDBAnimation type="fadeInRight" duration="1s">
+              <div className="right-panel">
+                <h1>
+                  <strong>Welcome Back,</strong> <br />
+                  Clashers!
+                </h1>
+                <form className="animated-form">
+                  <h3>Login to your account</h3>
+                  <div
+                    className={`form-group ${
+                      userObj?.email?.length > 0 ? "has-content" : ""
+                    }`}
+                  >
+                    <label htmlFor="email">Email</label>
+                    <input
+                      type="text"
+                      id="email"
+                      onChange={(e) =>
+                        setUserObj({ ...userObj, email: e.target.value })
+                      }
+                    />
+                  </div>
+                  <div
+                    className={`form-group ${
+                      userObj?.password?.length > 0 ? "has-content" : ""
+                    }`}
+                  >
+                    <label htmlFor="password">Password</label>
+                    <input
+                      type="password"
+                      id="password"
+                      onChange={(e) =>
+                        setUserObj({ ...userObj, password: e.target.value })
+                      }
+                    />
+                  </div>
+                  <div className="form-group flex-end">
+                    <button className="button">LOGIN</button>
+                  </div>
+                  <MDBAnimation type="fadeInUp" delay="1s">
+                    <div className="new-user">
+                      <img
+                        src={miniPekka}
+                        alt="mini pekka"
+                        className="clash-pic"
+                      />
+                      <hr className="line" />
+                      <h3>New Here?</h3>
+                      <p className="link" onClick={handleSignInLogin}>
+                        Sign up
+                      </p>
+                    </div>
+                  </MDBAnimation>
+                </form>
+              </div>
+            </MDBAnimation>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Login;
